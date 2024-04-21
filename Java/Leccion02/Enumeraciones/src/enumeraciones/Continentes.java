5f2bc06984a1a0c80a7cb96c80d3db95fee8b67c
Julieta Aguilera <aguilerajulieta700@gmail.com>
2024-04-21 11:29:36 -0300

    Se agrega la clase dos y tres de Java

---
+++ Java/Leccion02/Enumeraciones/src/enumeraciones/Continentes.java
@@ -0,0 +1,27 @@
+
+package enumeraciones;
+
+public enum Continentes {
+    AFRICA(53, "1.2 billones"),
+    EUROPA(76, "1.1 billones"),
+    ASIA(44, "1.9 billones"),
+    AMERICA(34, "150.2 millones"),
+    OCEANIA(14, "1.2 billones");
+    
+    private final int paises;
+    private String habitantes;
+    
+    Continentes(int paises, String habitantes){
+        this.paises = paises;
+        this.habitantes = habitantes;
+    }
+    
+    //Metodo get
+    public int getPaises(){
+        return this.paises;
+    }
+    
+    public String getHabitantes(){
+        return this.habitantes;
+    }
+}
