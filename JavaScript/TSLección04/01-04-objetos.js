4b80fbc50f36f949a1104162f572fbda2cce1a81
Julieta Aguilera <aguilerajulieta700@gmail.com>
2024-05-10 12:31:48 -0300

    metodos get y set, constructores y objetos

--- JavaScript/TSLección04/01-04-objetos.js
+++ JavaScript/TSLección04/01-04-objetos.js
@@ -8,9 +8,20 @@ let persona ={
     apellido: "Rodriguez",
     email: "romi@gmail.com",
     edad: 36,
+    idioma: 'es', 
+    get lang(){
+        return this.idioma.toUpperCase(); //Convierte las minusculas en mayusculas
+    },
+    set lang(lang){
+        return this.idioma = lang.toUpperCase();
+    },
     nombreCompleto: function(){//Método o función
         return this.nombre+" "+this.apellido;
+    },
+    get nombreEdad(){
+        return 'El nombre es: '+this.nombre+', Edad: '+this.edad;
     }
+    
 }
 console.log(persona.nombre);
 console.log(persona.apellido);
@@ -36,8 +47,8 @@ for(propiedad in persona){
 }
 console.log("comabiamos y eliminamos un error");
 
-persona.apellida = "Betancud";//cambiamos dinamicamente un valor del objeto
-delete persona.apellida;//borramos el error que teniamos
+persona.apellido = "Betancud";//cambiamos dinamicamente un valor del objeto
+delete persona.apellido;//borramos el error que teniamos
 console.log(persona);
 
 //Distinta formas de imprimir un objeto
@@ -61,4 +72,26 @@ console.log("//Distinta formas de imprimir un objeto: forma 4");
 let personaString = JSON.stringify(persona);
 console.log(personaString);
 
+console.log('Comenzamos a usar el metodo get');
+console.log(persona.nombreEdad);
 
+console.log('Comenzamos el metodo get y set para idiomas');
+persona.lang = 'en'
+console.log(persona.lang);
+
+function Persona3(nombre, apellido, email){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre+' '+this.apellido;
+    }
+}
+let padre = new Persona3('Leo', 'lopez', 'lopez@gmail.com');
+padre.nombre = 'Luis' // Modificamos el nombre
+console.log(padre);
+console.log(padre.nombreCompleto()); //Utilizamos la funcion
+
+let madre = new Persona3('Laura', 'Contrera', 'contrera@gmail.com');
+console.log(madre);
+console.log(madre.nombreCompleto());
