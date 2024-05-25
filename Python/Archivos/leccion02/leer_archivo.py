127731d6147cacdda42873eeff4e9061a55fbc0f
virginiacastellano <vcastellano140@gmail.com>
2024-05-24 21:53:27 -0300

    se sube la primer parte de python de la clase 6 Manejo de Archivos hasta el video 6, de la semana 7 por parte de virginia

---
+++ Python/Archivos/leccion02/leer_archivo.py
@@ -0,0 +1,6 @@
+archivo= open("prueba.txt","r", encoding="utf8")
+#print(archivo.read())
+# print(archivo.read(16))
+# print(archivo.read(10)) # continuamos desde la línea anterior
+print(archivo.readline())
+print(archivo.readline())
---
+++ Python/Archivos/leccion02/manejo_archivos.py
@@ -0,0 +1,15 @@
+#declaramos una variable 
+try:
+    archivo= open("prueba.txt", "w", encoding='utf8')
+    archivo.write("peogramamos con diferentes tipos de archivos, ahora en txt. \n")
+    archivo.write("los acentos son importantes para las palabtas. \n")
+    archivo.write("como por ejemplo: acción, ejecución y producción.\n")
+    archivo.write("las letras son: \nr de read leer, \na append anexa, \nw write escribe, \nx crea archivos")
+    archivo.write("\nt esta es para textos o text, \nb archivos binarios, \nw+ lee y escribe son iguales r+\n")
+    archivo.write("con esto terminamos")
+except Exception as e:
+    print(e)
+finally:
+    archivo.close()
+    
+#archivo.write("con esto terminamos") esto es un error
