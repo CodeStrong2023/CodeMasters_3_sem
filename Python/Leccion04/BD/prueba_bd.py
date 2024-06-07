1b7faf4bba113b2aa2d7654b64b79e637dcb3d25
Luciana Castro <cluciana615@gmail.com>
2024-06-07 10:39:47 -0300

    Se sube clase 8 Postgresql (vídeos 6 y 7) Python

--- Python/Leccion04/BD/prueba_bd.py
+++ Python/Leccion04/BD/prueba_bd.py
@@ -1,17 +1,21 @@
-import psycopg2 #esto es para poder conectarse a Postgre
+import psycopg2 # Con esto se conecta a Postgre
 
 conexion = psycopg2.connect(
-    user='postgres',
-    password='admin',
-    host='127.0.0.1',
-    database='test_db'
+    user="postgres",
+    password="admin",
+    host="127.0.0.1",
+    port="5432",
+    database="test_bd"
 )
 
+# print(conexion)
+
 cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia) # de esta menera ejecutamos la sentencia
-registros = cursor.fetchall() #Recuperamos todos los registros que seran una lista
-print(registros)
+sentencia = "SELECT * FROM persona"
+cursor.execute(sentencia) #Ejecutamos la sentencia
+registro = cursor.fetchall() #Recuperamos todos los registros que serán una lista
+
+print(registro)
 
 cursor.close()
 conexion.close()
